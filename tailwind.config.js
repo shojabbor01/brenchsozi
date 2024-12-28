bc43e1d4953ff66f5f182d28d8a3c9f03319e89c
shojabbor01 <jabborovshojabbor@gmail.com>
2024-12-28 13:31:55 +0500

    aded html files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+/ @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src//*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '320px',
+        'md': '640px',
+        'lg' : '768px',
+        'xl' : '1024px',
+        '2xl' : '1280px',
+      }
+    },
   },
   plugins: [],
 }
